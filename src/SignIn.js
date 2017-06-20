f82d585ea0c174b700b68bdc44996aa61295ca56
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-19 20:44:18 -0400

    Create visually appealing log in page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './SignIn.css'
 import { auth, githubProvider } from './base'
+import quill from './quill.svg'
 
-const SignIn = () => {
+class SignIn extends Component {
 
-    const authenticate = () => {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    authenticateGithub = () => {
         auth.signInWithPopup(githubProvider)
     }
 
-    return (
-        <button className="SignIn" onClick={authenticate}>
-            Sign in
-        </button>
-    )
+    authenticatePassword = () => {
+
+    }
+
+    registerUser = () => {
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <div className="loginLogo">
+                    <img src={quill} alt="Noteherder" />
+                </div>
+                <h1 className="loginTitle">NOTEHERDER</h1>
+                <h2 className="loginSubtitle">Take notes, beautifully.</h2>
+                <div className="loginForm">
+                    <form>
+                        <p className="formItem"><input type="text" name="email" placeholder="E-Mail" onChange={(event) => this.setState({email:event.target.value})}/></p>
+                        <p className="formItem"><input type="password" name="password" placeholder="Password" onChange={(event) => this.setState({password:event.target.value})}/></p>
+                        <button className="signIn" onClick={this.authenticatePassword}>
+                            <i className="fa fa-sign-in"/> Sign in
+                        </button>
+                        <button className="signUp" onClick={this.authenticatePassword}>
+                            <i className="fa fa-user-plus"/> Create account
+                        </button>
+                        <button className="signInGH" onClick={this.authenticateGithub}>
+                            <i className="fa fa-github"/> Sign in with GitHub
+                        </button>
+                    </form>
+                </div>
+            </div>
+            
+        )
+    }
+    
 }
 
 export default SignIn
